75767764a9d4953d3fe63e9c818f59ea8d594208
Muhammad Wasif Ijaz <62423571+wasifijaz@users.noreply.github.com>
2023-06-20 19:49:23 +0500

    Create app.py
    Flask API Added

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+@app.route('/nba-zip/')
+def return_files_nba():
+    try:
+        return send_file('/home/ubuntu/article-automation/NBA.zip')
+    except Exception as e:
+        return str(e)
+    
+@app.route('/nhl-zip/')
+def return_files_nhl():
+    try:
+        return send_file('/home/ubuntu/article-automation/NHL.zip')
+    except Exception as e:
+        return str(e)
+
+
+if __name__ == "__main__":
+    app.run()
